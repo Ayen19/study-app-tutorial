870e1d77a6277471641ba960569896b717d466aa
stephen <stephen@actsmedia.com>
2021-11-12 03:46:50 -0500

    rename studyapp_tutorial to study-app-tutorial

--- study-app-tutorial/urls.py
+++ study-app-tutorial/urls.py
@@ -1,4 +1,4 @@
-"""studyapp_tutorial URL Configuration
+"""study-app-tutorial URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
